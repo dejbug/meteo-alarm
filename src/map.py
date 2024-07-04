17a843b5a110616d3d313f263c8eb4d71325f219
Dejan Budimir <dejbug@users.noreply.github.com>
2024-07-04 12:07:57 +0200

    fetcher redesign

--- src/map.py
+++ src/map.py
@@ -1,13 +1,16 @@
+__version__ = '1.0.0'
+
 import kivy
 kivy.require('2.3.0')
 
 from kivy.config import Config
-Config.set('graphics', 'width', 400)
-Config.set('graphics', 'height', 600)
-Config.set('input', 'mouse', 'mouse,disable_multitouch')
+if kivy.platform != 'android':
+	Config.set('graphics', 'width', 400)
+	Config.set('graphics', 'height', 600)
+	Config.set('input', 'mouse', 'mouse,disable_multitouch')
 
 from kivy.app import App
-from kivy.core.window import Window
+# from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
 # from kivy.uix.image import Image
@@ -16,12 +19,128 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, Rectangle
 from kivy.properties import StringProperty, DictProperty
 from kivy.clock import Clock
-
 from kivy.gesture import GestureDatabase, Gesture
+from kivy.storage.jsonstore import JsonStore
 
 import os, time, threading, urllib.error
 
 import scraper
+
+
+class WarningsFetcher2:
+
+	STATE_NONE, STATE_FETCHING, STATE_OK, STATE_FAIL = range(4)
+
+	def __init__(self):
+		self.runner = None
+		self.ticker = None
+
+		self.ticks = 0
+		self.state = 0
+		self.warnings = None
+
+	@property
+	def fetching(self):
+		return self.state == self.STATE_FETCHING
+
+	@property
+	def ok(self):
+		return self.state == self.STATE_OK
+
+	def start(self):
+		if self.runner:
+			return False
+		self.runner = threading.Thread(target = self.run, daemon = True)
+		self.runner.start()
+		return True
+
+	def run(self):
+		self.warnings = None
+		self.state = self.STATE_FETCHING
+
+		self.ticks = 0
+		self.ticker = threading.Timer(1, self.tick)
+		self.ticker.start()
+		self.url, self.warnings = self.fetch_warnings()
+		self.ticker.cancel()
+
+		self.state = self.STATE_OK if self.warnings else self.STATE_FAIL
+
+	def tick(self):
+		self.ticks += 1
+		if self.state == self.STATE_FETCHING:
+			self.ticker = threading.Timer(1, self.tick)
+			self.ticker.start()
+
+	@classmethod
+	def fetch_warnings(cls, dt = None):
+		url = scraper.mkurl(dt)
+		text = scraper.fetch(url)
+		rr = {}
+		if text:
+			text = text.decode('utf8')
+			rr = scraper.iter_regions(text)
+			rr = list(rr); print(rr)
+			rr = { r[0] : r[1] for r in rr }
+		return url, rr
+
+
+class Refresher2:
+
+	MAXAGE = 3600
+
+	STATE_OK_CACHED = 4
+
+	def __init__(self, view, store = 'store.json'):
+		assert isinstance(view, MapView)
+		self.view = view
+		self.store = JsonStore(store)
+		self.fetcher = None
+		self.timer = None
+		self.last_state = 0
+
+	def start(self):
+		if self.fetcher:
+			print('Refresher: already running')
+			return False
+
+		if self.store.exists('warnings'):
+			warnings = self.store.get('warnings')
+			timestamp = warnings['timestamp']
+			age = time.time() - timestamp
+			if age < self.MAXAGE:
+				self.view.on_fetcher_state_change(self.STATE_OK_CACHED, warnings['warnings'])
+				self.fetcher = None
+				print('Refresher: ok (cache)')
+				return True
+
+		print('Refresher: starting')
+		self.fetcher = WarningsFetcher2()
+		self.fetcher.start()
+		self.timer = Clock.schedule_interval(self.on_timer, 0.25)
+		return True
+
+	def cancel(self):
+		if self.fetcher:
+			self.fetcher = None
+		if self.timer:
+			self.timer.cancel()
+			self.timer = None
+
+	def on_timer(self, *aa):
+		print('tick', aa)
+
+		if not self.fetcher:
+			self.timer.cancel()
+			return
+
+		if self.last_state != self.fetcher.state:
+			self.last_state = self.fetcher.state
+			self.view.on_fetcher_state_change(self.last_state, self.fetcher.warnings)
+			if self.fetcher.ok:
+				self.store.put('warnings', warnings = self.fetcher.warnings, url = self.fetcher.url, timestamp = time.time())
+			if not self.fetcher.fetching:
+				self.fetcher = None
 
 
 class WarningsFetcher(threading.Thread):
@@ -69,8 +188,10 @@ class WarningsFetcher(threading.Thread):
 	@classmethod
 	def fetch_warnings(cls, dt = None, cache_root = '.'):
 		cache = scraper.Cache(root = cache_root)
-		url = scraper.mkurl('2024-6-12')
-		text = cache.fetch(url, maxage = 3600)
+		# url = scraper.mkurl('2024-6-12')
+		# text = cache.fetch(url, maxage = 3600)
+		url = scraper.mkurl()
+		text = cache.fetch(url, maxage = 0)
 		if text:
 			rr = scraper.iter_regions(text)
 			# rr = list(rr); print(rr)
@@ -209,13 +330,18 @@ class MapView(GestureWidget):
 			size = self.on_size_change,
 			warnings = self.on_warnings_change)
 
-		Window.bind(on_mouse_down = self.on_mouse_down)
+		# Window.bind(on_mouse_down = self.on_mouse_down)
 
-		self.refresher = Refresher(self)
+		self.refresher = Refresher2(self)
 		self.refresher.start()
 
 	# def on_refresh_ok(self, *aa):
 	# 	self.warnings = self.fetcher.warnings
+
+	def on_fetcher_state_change(self, state, warnings):
+		print(state, warnings)
+		if warnings:
+			self.warnings = warnings
 
 	def on_warnings_change(self, *aa):
 		for id, ww in self.warnings.items():
@@ -236,8 +362,8 @@ class MapView(GestureWidget):
 			r.pos = (self.pos[0], self.size[1] - h)
 			r.size = (w, h)
 
-	def on_mouse_down(self, win, x, y, button, modifiers):
-		print(x, y, button, modifiers)
+	# def on_mouse_down(self, win, x, y, button, modifiers):
+	# 	print(x, y, button, modifiers)
 
 	def set_region_color(self, id, rgba):
 		self.region_colors[id].rgba = rgba
